1b99bfde0fb719264f7c5b3220a12e1dfb1e4aca
kwon <koh9037@gmail.com>
2021-10-26 08:36:43 +0900

    데이터받아오기

--- LotteryProject/ViewController.swift
+++ LotteryProject/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        getLotto()
     }
 
-
+    func getLotto(){
+        
+        let url = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=903"
+        //비공개
+        
+        AF.request(url, method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value): //성공했을 때(let 연관값)
+                let json = JSON(value)
+                print("JSON: \(json)")
+            case .failure(let error): //실패했을 때
+                print(error)
+            }
+        }
+    }
 }
 
